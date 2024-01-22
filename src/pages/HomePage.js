4064aedf92fc0a6508a44b6b632afabfcd9cb76b
Bryson2809 <jbc2@email.sc.edu>
2024-01-22 00:53:36 -0500

    added cover photo

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,10 +1,15 @@
 import React from "react";
+
+import cow from "../images/cow.jpg";
 //import { useState, useEffect } from "react";
 
 const HomePage = () => {
     return (
         <div>
             Home Page
+            <div className="coverPhoto">
+                <img src={cow} alt="cow in front of CFA Hillcrest" />
+            </div>
         </div>
     );
 }
