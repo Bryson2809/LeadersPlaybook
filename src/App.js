303b7e1063e309701ac4ec463aa59d303cb8f8ef
Bryson2809 <jbc2@email.sc.edu>
2024-01-24 15:47:09 -0500

    added react to app.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 // Import the functions you need from the SDKs you need
+import React from "react";
 import { Route, Routes } from "react-router-dom";
 
 import { initializeApp } from "firebase/app";
