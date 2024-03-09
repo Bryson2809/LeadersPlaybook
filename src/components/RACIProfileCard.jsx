b3c2f86a593cbec00f10b77d0742aa1a4ffa9425
Bryson2809 <jbc2@email.sc.edu>
2024-03-08 20:01:23 -0500

    Toggle role functioning

--- src/components/RACIProfileCard.jsx
+++ src/components/RACIProfileCard.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useState } from "react";
 
 import { db } from "../utils/firebase";
@@ -9,30 +9,90 @@ import { getDoc, updateDoc, doc, collection } from "firebase/firestore";
 
 const RACIProfleCard = (props) => {
     const [name, setName] = useState("");
+    const [r, setR] = useState(true);
+    const [a, setA] = useState(true);
+    const [c, setC] = useState(true);
+    const [i, setI] = useState(true);
 
-    // const toggleRole = async () => {
-    //     var currClass = $(this).hasClass("test");
-    //     console.log(currClass);
-    // }
+    useEffect(() => {
+        const fetchRoles = async () => {
+            const role = await getDoc(doc(db, "playbooks", "Kxla6slB7RIc19lJ4qWI", "RACIProfiles", "EhDdmAgrPsC4PiGwvwVc"));
+            console.log(`Initial render ${role.data().R}`);
+            if (role.data().R === true) {
+                document.getElementById("r").className = "selected";
+                setR(true);
+            }
+            else if (role.data().R === false) {
+                document.getElementById("r").className = "";
+                setR(false);
+            }
+
+            if (role.data().A === true) {
+                document.getElementById("a").className = "selected";
+                setA(true);
+            }
+            else if (role.data().A === false) {
+                document.getElementById("a").className = "";
+                setA(false);
+            }
+
+            if (role.data().C === true) {
+                document.getElementById("c").className = "selected";
+                setC(true);
+            }
+            else if (role.data().C === false) {
+                document.getElementById("c").className = "";
+                setC(false);
+            }
+
+            if (role.data().I === true) {
+                document.getElementById("i").className = "selected";
+                setI(true);
+            }
+            else if (role.data().I === false) {
+                document.getElementById("i").className = "";
+                setI(false);
+            }
+            console.log(r);
+        }
+        fetchRoles();
+    });
 
     const toggleRole = async (event) => {
-        var currClass = event.currentTarget.className;
-
-        if (currClass === "selected") {
-            event.currentTarget.className = "";
-        }
-        else {
-            event.currentTarget.className = "selected";
-        }
-
         var role = event.currentTarget.id;
+        const docSnap = await getDoc(doc(db, "playbooks", "Kxla6slB7RIc19lJ4qWI", "RACIProfiles", "EhDdmAgrPsC4PiGwvwVc"));
+
         const docRef = doc(db, "playbooks", "Kxla6slB7RIc19lJ4qWI", "RACIProfiles", "EhDdmAgrPsC4PiGwvwVc");
-        console.log(docRef.R);
 
         if (role === "r") {
+            var newData = !docSnap.data().R;
             await updateDoc(docRef, {
-                R: !docRef.R,
+                R: newData,
             });
+            console.log("toggled R");
+            setR(!r);
+            console.log(r);
+        }
+        else if (role === "a") {
+            await updateDoc(docRef, {
+                A: !docSnap.data().A,
+            });
+            console.log("toggled A");
+            setA(!a);
+        }
+        else if (role === "c") {
+            await updateDoc(docRef, {
+                C: !docSnap.data().C,
+            });
+            console.log("toggled C");
+            setC(!c);
+        }
+        else if (role === "i") {
+            await updateDoc(docRef, {
+                I: !docSnap.data().I,
+            });
+            console.log("toggled I");
+            setI(!i);
         }
     }
 
